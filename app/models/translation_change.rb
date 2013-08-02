411890154c2619f24e9dc0a912d27fdd7c132711
Wenley Tong <wenley@squareup.com>
2013-08-02 13:24:03 -0700

    Add comments for TranslationChange and TranslationObserver

--- app/models/translation_change.rb
+++ app/models/translation_change.rb
@@ -1,4 +1,25 @@
-# TODO (wenley, tim) Add comments
+# A change in a {Translation}. Currently tracks a {Translation}'s `copy` and
+# `approved` fields. A TranslationChange exists for every time a {Translation}
+# is edited.
+#
+# Associations
+# ============
+#
+# |               |                                     |
+# |:--------------|:------------------------------------|
+# | `translation` | The {Translation} that was changed. |
+# | `user`        | The {User} that made the change.    |
+#
+# Properties
+# ==========
+#
+# Metadata
+# ========
+#
+# |        |                           |
+# |:-------|:--------------------------|
+# | `diff` | The changes that occured. |
+
 class TranslationChange < ActiveRecord::Base
   belongs_to :translation, inverse_of: :translation_changes
   belongs_to :user
@@ -15,8 +36,12 @@ class TranslationChange < ActiveRecord::Base
     return unless translation.translated_changed? || translation.approved_changed? || translation.copy_changed?
     change = TranslationChange.new(translation: translation, user: translation.modifier)
     diff = translation.changes.slice("copy", "approved")
+
+    # Necessary due to assign_attributes pushing back the cache; see TranslationsController#update
     diff["copy"] = [translation.copy_actually_was, translation.copy] if translation.copy_actually_was
     diff = diff.select { |k,v| v[0] != v[1] } # Filter for duplicates
+    return if diff.empty?
+
     change.diff = diff
     change.save!
     change.freeze
