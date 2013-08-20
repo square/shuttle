b34485fd1d2108257646705d4c0b6ddb7363e8ab
Wenley Tong <wenley@squareup.com>
2013-08-20 13:43:50 -0700

    Refactored to use MethodFinder in the gem

--- lib/importer/nt_ruby.rb
+++ lib/importer/nt_ruby.rb
@@ -18,11 +18,6 @@ require 'natural_translation/importer/method_finder'
 module Importer
 
   # Parses translatable strings from Ruby `.rb` files.
-  #
-  # WARNING: Only recognizes String literals (single- or double-quoted)
-  # without any parentheses in their content
-  #
-  # TODO (wenley, tim) : Expand recognition beyond simple regex's
 
   class NtRuby < Base
     include NtBase
@@ -36,107 +31,9 @@ module Importer
     end
 
     def import_strings(receiver)
-      nt_calls_in(file.contents).each do |message, comment|
+      MethodFinder.new(file.contents).args_to("nt")[0..1].each do |message, comment|
         receiver.add_nt_string(key_for(message, comment), message, comment)
       end
-    end
-
-    private
-
-    # @return [Array<Array<String>>] Returns the string and comments of each
-    #   call to nt
-    def nt_calls_in(source)
-      tree = Ripper.sexp(source)
-      calls = find_nt_in_parse_tree(tree).map { |subtree| Ripper.hashify(subtree) }
-      calls.map do |subtree|
-        args = get_args(subtree)
-        args[0..1].map { |arg_tree| arg_tree[:string_literal][:string_content][:@tstring_content][0] }
-      end
-    end
-
-    def find_nt_in_parse_tree(tree)
-      calls = []
-      if Enumerable === tree
-        tree.each do |subtree|
-          if Symbol === subtree
-            next
-          elsif Array === subtree && is_nt_call?(subtree)
-            calls.append(subtree)
-          else
-            calls += find_nt_in_parse_tree(subtree)
-          end
-        end
-      end
-      calls
-    end
-
-    def get_args(subtree)
-      if sub = (subtree[:command] || subtree[:command_call])
-        sub[:args_add_block]
-      elsif subtree[:method_add_arg] && subtree[:method_add_arg][:arg_paren]
-        subtree[:method_add_arg][:arg_paren][:args_add_block]
-      end
-    end
-
-    def is_nt_call?(subtree)
-      (recognizer = syntax_recognizers[subtree[0]]) &&
-        recognizer.call(subtree)
-    end
-
-    def syntax_recognizers
-      {
-        :command => proc { |subtree|
-            if Array === subtree
-              subtree[1][1] == "nt"
-            elsif Hash === subtree
-              subtree[:command][:@ident][0] == "nt"
-            end
-        },
-        :command_call => proc { |subtree|
-            if Array === subtree
-              subtree[3][1] == "nt"
-            elsif Hash === subtree
-              subtree[:command][3][:@ident][0] == "nt"
-            end
-        },
-        :method_add_arg => proc { |subtree|
-            if Array === subtree
-              subtree[1][1][1] == "nt" || # nt(something)
-                subtree[1][3][1] == "nt"  # NaturalTranslation.nt(something)
-            elsif Hash === subtree
-              subtree[:method_add_arg][:fcall][:@ident][0] == "nt" ||  # nt(something)
-                subtree[:method_add_arg][:call][3][:@ident][0] == "nt" # NaturalTranslation.nt(something)
-            end
-        },
-      }
-    end
-  end
-end
-
-class Ripper
-  # @private
-  # Makes accessing subtree components much more readable
-  #
-  # @param [Array<Array<...>>] Nested arrays representing a syntax tree
-  # @return [Hash<Hash, Array, ...>] Nested hashes representing the same tree
-  def self.hashify(subtree)
-    if ! subtree.present?
-      subtree
-    elsif Symbol === subtree[0]
-      val = subtree[1..subtree.length].map { |sub| hashify(sub) }.reduce { |memo, obj|
-        if Hash === memo && Hash === obj # Recursive case
-          memo.merge(obj)
-        elsif Array === memo # Base case extended
-          memo.push(obj)
-        else # Base case
-          [memo, obj]
-        end
-      }
-      { subtree[0] => val }
-    elsif Array === subtree
-      subtree.map { |obj| hashify(obj) }
-    else
-      subtree
     end
   end
 end
