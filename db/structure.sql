28e861da01a2111b4c4df98201b6eea08fea6fda
Tim Morgan <tim@squareup.com>
2013-11-12 14:26:14 -0800

    Revert "Revert "Merge branch 'rekey-android'""
    This reverts commit 7352d2b6ecdd0e9e29f4ff155e3e49d2cb1fbed1.
    
    Conflicts:
    	lib/importer/android.rb
    	lib/localizer/android.rb
    	spec/lib/importer/android_spec.rb

--- db/structure.sql
+++ db/structure.sql
@@ -973,4 +973,6 @@ INSERT INTO schema_migrations (version) VALUES ('20130821011614');
 
 INSERT INTO schema_migrations (version) VALUES ('20131008220117');
 
+INSERT INTO schema_migrations (version) VALUES ('20131031034100');
+
 INSERT INTO schema_migrations (version) VALUES ('20131111213136');
