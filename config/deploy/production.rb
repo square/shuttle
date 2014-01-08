3fc45592134554fffdd05d7e11403a6a46d78e28
Rick Song <ricksong@squareup.com>
2014-01-08 00:51:04 -0800

    Adds new worker boxes to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 
-role :app, %w{square@ironweed.corp.squareup.com square@ginger.corp.squareup.com}
-role :web, %w{square@ironweed.corp.squareup.com}
+role :app, %w{square@ironweed.corp.squareup.com square@ginger.corp.squareup.com square@shuttle-web01.corp.squareup.com square@shuttle-web02.corp.squareup.com square@shuttle-worker01.corp.squareup.com square@shuttle-worker02.corp.squareup.com square@shuttle-worker03.corp.squareup.com square@shuttle-worker04.corp.squareup.com square@shuttle-worker05.corp.squareup.com}
+role :web, %w{square@shuttle-web01.corp.squareup.com square@shuttle-web02.corp.squareup.com}
 role :db,  %w{square@ironweed.corp.squareup.com}
