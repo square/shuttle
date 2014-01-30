3666c65378ef4a118f43f4195559f7ad189de0ee
Tim Morgan <tim@squareup.com>
2014-01-29 16:58:35 -0800

    Deploy tweak

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ worker_boxes = (1..5).map { |i| "square@shuttle-worker#{i.to_s.rjust(2, '0')}.co
 web_boxes    = (1..2).map { |i| "square@shuttle-web#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 db_boxes     = %w{square@ironweed.corp.squareup.com}
 
-role :app, web_boxes + worker_boxes + db_boxes
+role :app, (web_boxes + worker_boxes + db_boxes).uniq
 role :web, web_boxes
 role :db, db_boxes
 role :sidekiq, worker_boxes
