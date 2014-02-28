caae81dd59f7baaa7dae7bbd608cad7b493a0a21
Rick Song <ricksong@squareup.com>
2014-02-28 11:26:29 -0800

    Fixed minor Flash constantly appears issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,6 @@ Shuttle::Application.configure do
 
   # Don't care if the mailer can't send. TODO: Still necessary?
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation          = :log
