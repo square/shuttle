87446ab7ae657c87bb7d6cb67f04c169f26105fe
Tim Morgan <git@timothymorgan.info>
2014-06-25 13:13:24 -0700

    Hostname change

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :stage, :staging
 
-role :app, %w{square@baltimore.corp.squareup.com}
-role :web, %w{square@baltimore.corp.squareup.com}
-role :db, %w{square@baltimore.corp.squareup.com}
-role :sidekiq, %w{square@baltimore.corp.squareup.com}
-role :cron, %w{square@baltimore.corp.squareup.com}
+role :app, %w{square@shuttle-stage-a-01.corp.squareup.com}
+role :web, %w{square@shuttle-stage-a-01.corp.squareup.com}
+role :db, %w{square@shuttle-stage-a-01.corp.squareup.com}
+role :sidekiq, %w{square@shuttle-stage-a-01.corp.squareup.com}
+role :cron, %w{square@shuttle-stage-a-01.corp.squareup.com}
