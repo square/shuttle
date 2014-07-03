3cc2eba6e98a545ccb624a885d045e741791c306
Yunus Sasmaz <yunus@squareup.com>
2014-07-03 12:59:02 -0700

    Fix specs
    Conflicts:
    	spec/controllers/registrations_controller_spec.rb

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -15,5 +15,45 @@
 require 'spec_helper'
 
 describe RegistrationsController do
+  before(:all) { User.delete_all }
+  before(:each) { @request.env["devise.mapping"] = Devise.mappings[:user] }
 
+  describe '#create' do
+    it "should require email confirmation for a Square email address" do
+      post :create, user: {
+          email:                 'foo@squareup.com',
+          first_name:            'Sancho',
+          last_name:             'Sample',
+          password:              'test123',
+          password_confirmation: 'test123'
+      }
+      user = User.last
+      expect(user.email).to eql('foo@squareup.com')
+      expect(user.confirmed_at).to be_nil
+    end
+
+    it "should otherwise not require email confirmation but create an inactive account" do
+      post :create, user: {
+          email:                 'foo@bar.com',
+          first_name:            'Sancho',
+          last_name:             'Sample',
+          password:              'test123',
+          password_confirmation: 'test123'
+      }
+      user = User.last
+      expect(user.email).to eql('foo@bar.com')
+      expect(user.confirmed_at).not_to be_nil
+    end
+
+    it "should handle bogus email addresses" do
+      post :create, user: {
+          email:                 'foo123',
+          first_name:            'Sancho',
+          last_name:             'Sample',
+          password:              'test123',
+          password_confirmation: 'test123'
+      }
+      expect(User.count).to be_zero
+    end
+  end
 end
