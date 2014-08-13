9591db79ae6cb3d6baeb8c56a1d1789164c21bc5
Rick Song <ricksong@squareup.com>
2014-08-12 21:54:22 -0700

    Adds Daily Metric specs

--- spec/models/daily_metric_spec.rb
+++ spec/models/daily_metric_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
 
 describe DailyMetric do
+  context "[validations]" do
+    it "validates presence of date" do
+      metric = FactoryGirl.build :daily_metric, date: nil
+      expect(metric).to_not be_valid
+    end
+
+    metric_fields = [:num_commits_loaded,
+                     :num_commits_loaded_per_project,
+                     :avg_load_time,
+                     :avg_load_time_per_project,
+                     :num_commits_completed,
+                     :num_commits_completed_per_project,
+                     :num_words_created,
+                     :num_words_created_per_language,
+                     :num_words_completed,
+                     :num_words_completed_per_language]
+    metric_fields.each do |field|
+      it "validates presence of #{field}" do
+        metric = FactoryGirl.build :daily_metric, field => nil
+        expect(metric).to_not be_valid
+      end
+    end
+
+    it "should be valid if all required fields are present" do
+      metric = FactoryGirl.build :daily_metric
+      expect(metric).to be_valid
+    end
+  end
 end
