911395bea938db13109c4f6669b8adf62e7b3f15
Yunus Sasmaz <yunus@squareup.com>
2014-08-26 14:05:06 -0700

    Add new worker boxes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 
-worker_boxes = (1..3).map { |i| "square@shuttle-worker#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
+worker_boxes = (1..3).map { |i| "square@shuttle-worker#{i.to_s.rjust(2, '0')}.corp.squareup.com" } + (1..5).map { |i| "square@shuttle-worker-a-#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 web_boxes    = (1..2).map { |i| "square@shuttle-web#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 db_boxes     = %w{square@ironweed.corp.squareup.com}
 
