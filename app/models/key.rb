1f4004e86de51bfdaf6b2e2c178967c0156c70a2
Yunus Sasmaz <yunus@squareup.com>
2014-10-12 22:51:21 -0700

    Run recalculate_readiness after destroy only if apply_readiness_hooks is true

--- app/models/key.rb
+++ app/models/key.rb
@@ -239,6 +239,7 @@ class Key < ActiveRecord::Base
   def remove_excluded_pending_translations
     translations.not_base.not_translated.where(approved: nil).find_each do |translation|
       if skip_key?(translation.locale) || !targeted_locales.include?(translation.locale)
+        translation.skip_readiness_hooks = true
         translation.destroy
       end
     end
