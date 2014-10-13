532bdbde9d83ca26ce93a6a274dcc06291509032
Yunus Sasmaz <yunus@squareup.com>
2014-10-13 13:44:07 -0700

    Partition TMC to run in parallel

--- spec/workers/translations_mass_copier_spec.rb
+++ spec/workers/translations_mass_copier_spec.rb
@@ -24,17 +24,19 @@ describe TranslationsMassCopier do
       @en_xx_translation = FactoryGirl.create(:translation, key: @key, source_copy: "fake", source_rfc5646_locale: 'en', rfc5646_locale: 'en-XX', copy: nil, approved: nil, skip_readiness_hooks: true)
     end
 
-    it "copies translation.copy from the given source locale to given target locale" do
-      TranslationsMassCopier.new.perform(@project.id, 'en', 'en-XX')
-      expect(@en_xx_translation.reload.copy).to eql('test')
+    it "errors if there is a problem with the inputs such as locales not being in the same language family" do
+      expect { TranslationsMassCopier.new.perform(@project.id, 'en', 'es-XX') }.to raise_error(ArgumentError, "Source and target locales are not in the same language family (their ISO639s do not match)")
     end
 
-    it "sets approved state to true" do
-      TranslationsMassCopier.new.perform(@project.id, 'en', 'en-XX')
-      expect(@en_xx_translation.reload.approved).to be_true
+    it "should not run TranslationCopier or create a batch if all translations are already done in the target locale" do
+      @en_xx_translation.update! copy: "test"
+      tmc = TranslationsMassCopier.new
+      expect(tmc).to_not receive(:mass_copier_batch)
+      expect(TranslationCopier).to_not receive(:perform_once)
+      tmc.perform(@project.id, 'en', 'en-XX')
     end
 
-    it "updates keys and commits readiness states" do
+    it "copies translations, and updates keys & commits readiness states" do
       commit = FactoryGirl.create(:commit, project: @project, ready: false)
       commit.keys << @key.reload
       expect(commit.ready).to be_false
@@ -43,44 +45,6 @@ describe TranslationsMassCopier do
       expect(@en_xx_translation.reload.copy).to eql('test')
       expect(commit.reload.ready).to be_true
       expect(@key.reload.ready).to be_true
-    end
-
-    it "errors if there is a problem with the inputs such as locales not being in the same language family" do
-      expect { TranslationsMassCopier.new.perform(@project.id, 'en', 'es-XX') }.to raise_error(ArgumentError, "Source and target locales are not in the same language family (their ISO639s do not match)")
-    end
-
-    it "doesn't copy from not-approved Translation" do
-      FactoryGirl.create(:translation, key: @key, source_rfc5646_locale: 'en', rfc5646_locale: 'en-YY', copy: 'test', approved: nil)
-      TranslationsMassCopier.new.perform(@project.id, 'en-YY', 'en-XX')
-      expect(@en_xx_translation.reload.copy).to be_nil
-      expect(@en_xx_translation.approved).to be_nil
-    end
-
-    it "doesn't copy to translated Translation" do
-      @en_xx_translation.update! copy: 'helloworld', approved: nil
-      TranslationsMassCopier.new.perform(@project.id, 'en', 'en-XX')
-      expect(@en_xx_translation.reload.copy).to eql('helloworld')
-      expect(@en_xx_translation.approved).to be_nil
-    end
-
-    it "doesn't copy to base Translation" do
-      @en_xx_translation.update! copy: 'helloworld', approved: true
-      expect { TranslationsMassCopier.new.perform(@project.id, 'en-XX', 'en') }.to raise_error
-      expect(@en_translation.reload.copy).to eql("test")
-      expect(@en_translation.approved).to be_true
-    end
-
-    it "ignores translations in other source locales than the project" do
-      FactoryGirl.create(:translation, key: @key, source_rfc5646_locale: 'en-ZZ', rfc5646_locale: 'en-YY', copy: 'test', approved: true)
-      TranslationsMassCopier.new.perform(@project.id, 'en-YY', 'en-XX')
-      expect(@en_xx_translation.reload.copy).to be_nil
-      expect(@en_xx_translation.approved).to be_nil
-    end
-
-    it "will not update the translation if it cannot find the from_translation (maybe that key was blacklisted in that source locale)" do
-      TranslationsMassCopier.new.perform(@project.id, 'en-YY', 'en-XX')
-      expect(@en_xx_translation.reload.copy).to be_nil
-      expect(@en_xx_translation.approved).to be_nil
     end
   end
 
@@ -155,6 +119,71 @@ describe TranslationsMassCopier do
     it "returns errors if project translation adder is still running" do
       @project.stub(:translation_adder_batch_status).and_return(double("batch_status"))
       expect(TranslationsMassCopier.find_locale_errors(@project, 'en', 'en-XX')).to eql(["Project Translation Adder batch is still running. Try after it finishes."])
+    end
+  end
+
+  describe "#key_ids_with_copyable_translations" do
+    it "returns ids of keys for which we can copy translations from the the source locale to the target locale" do
+      project = FactoryGirl.create(:project)
+      # create a copyable key
+      key1 = FactoryGirl.create(:key, project: project)
+      fr_translation_1    = FactoryGirl.create(:translation, key: key1, rfc5646_locale: 'fr', copy: 'test', approved: true, skip_readiness_hooks: true)
+      fr_xx_translation_1 = FactoryGirl.create(:translation, key: key1, rfc5646_locale: 'fr-XX', copy: nil, approved: nil, skip_readiness_hooks: true)
+
+      # create another copyable key
+      key2 = FactoryGirl.create(:key, project: project)
+      fr_translation_2    = FactoryGirl.create(:translation, key: key2, rfc5646_locale: 'fr', copy: 'test', approved: true, skip_readiness_hooks: true)
+      fr_xx_translation_2 = FactoryGirl.create(:translation, key: key2, rfc5646_locale: 'fr-XX', copy: nil, approved: nil, skip_readiness_hooks: true)
+
+      # create a not-copyable key because translation in the target locale is already done
+      key3 = FactoryGirl.create(:key, project: project)
+      fr_translation_3    = FactoryGirl.create(:translation, key: key3, rfc5646_locale: 'fr',    copy: 'test', approved: true, skip_readiness_hooks: true)
+      fr_xx_translation_3 = FactoryGirl.create(:translation, key: key3, rfc5646_locale: 'fr-XX', copy: 'test', approved: true, skip_readiness_hooks: true)
+
+      # create a not-copyable key because translation in the source locale is not approved
+      key4 = FactoryGirl.create(:key, project: project)
+      fr_translation_4    = FactoryGirl.create(:translation, key: key4, rfc5646_locale: 'fr',    copy: 'test', approved: nil, skip_readiness_hooks: true)
+      fr_xx_translation_4 = FactoryGirl.create(:translation, key: key4, rfc5646_locale: 'fr-XX', copy: nil,    approved: nil, skip_readiness_hooks: true)
+
+      # create a not-copyable key because translation in the source locale does not exist
+      key5 = FactoryGirl.create(:key, project: project)
+      fr_xx_translation_5 = FactoryGirl.create(:translation, key: key5, rfc5646_locale: 'fr-XX', copy: nil, approved: nil, skip_readiness_hooks: true)
+
+      # create a not-copyable key because translation in the target locale does not exist
+      key6 = FactoryGirl.create(:key, project: project)
+      fr_translation_6 = FactoryGirl.create(:translation, key: key6, rfc5646_locale: 'fr', copy: 'test', approved: true, skip_readiness_hooks: true)
+
+      expect(TranslationsMassCopier.new.key_ids_with_copyable_translations(project, 'fr', 'fr-XX').sort).to eql([key1.id, key2.id].sort)
+    end
+  end
+
+  describe "#mass_copier_batch" do
+    it "returns a batch with the correct description which calls TranslationsMassCopier::Finisher (& BatchKeyAndCommitRecalculator) on success" do
+      project = FactoryGirl.create(:project)
+      batch = TranslationsMassCopier.new.mass_copier_batch(project.id, 'en', 'en-XX')
+      expect(batch).to be_a_kind_of(Sidekiq::Batch)
+      expect(batch.description).to eql("Translations Mass Copier #{project.id} (en -> en-XX)")
+      TranslationsMassCopier::Finisher.any_instance.should_receive(:on_success).with(anything(), {'project_id' => project.id}).and_call_original
+      expect(BatchKeyAndCommitRecalculator).to receive(:perform_once).with(project.id)
+      batch.jobs {}
+    end
+
+    it "returns a new batch with a new bid" do
+      project = FactoryGirl.create(:project)
+      batch = TranslationsMassCopier.new.mass_copier_batch(project.id, 'en', 'en-XX')
+      initial_bid = batch.bid
+      batch = TranslationsMassCopier.new.mass_copier_batch(project.id, 'en', 'en-XX')
+      expect(batch.bid).to_not eql(initial_bid)
+    end
+  end
+end
+
+describe TranslationsMassCopier::Finisher do
+  describe "#on_success" do
+    it "calls BatchKeyAndCommitRecalculator with project id" do
+      project = FactoryGirl.create(:project)
+      expect(BatchKeyAndCommitRecalculator).to receive(:perform_once).with(project.id)
+      TranslationsMassCopier::Finisher.new.on_success(nil, { 'project_id' => project.id })
     end
   end
 end
