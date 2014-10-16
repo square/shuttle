8f583bcfbd1cd722d44c0377464c335b2922a763
Yunus Sasmaz <yunus@squareup.com>
2014-10-15 20:55:24 -0700

    Change web boxes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 worker_boxes = (1..3).map { |i| "square@shuttle-worker#{i.to_s.rjust(2, '0')}.corp.squareup.com" } + (1..5).map { |i| "square@shuttle-worker-a-#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
-web_boxes    = (1..2).map { |i| "square@shuttle-web#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
+web_boxes    = (1..2).map { |i| "square@shuttle-web-a-#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 db_boxes     = %w{square@ironweed.corp.squareup.com}
 
 role :app, (web_boxes + worker_boxes + db_boxes).uniq
