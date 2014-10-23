6a1389db1350fa0f4b2202abd7cab7c17ee776ad
Yunus Sasmaz <yunus@squareup.com>
2014-10-22 18:56:24 -0700

    Make User lockable (devise)

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,7 +58,7 @@ class User < ActiveRecord::Base
   ROLES = %w(monitor translator reviewer admin)
 
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
 
   has_many :authored_translations, class_name: 'Translation', foreign_key: 'translator_id', inverse_of: :translator, dependent: :nullify
   has_many :reviewed_translations, class_name: 'Translation', foreign_key: 'reviewer_id', inverse_of: :reviewer, dependent: :nullify
