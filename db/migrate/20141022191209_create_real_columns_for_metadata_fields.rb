b4c37fa20713716e89065c4a6e2e9a9ed1e7f091
Yunus Sasmaz <yunus@squareup.com>
2014-11-18 18:22:58 -0800

    Backwards compatibility with open source branch

--- db/migrate/20141022191209_create_real_columns_for_metadata_fields.rb
+++ db/migrate/20141022191209_create_real_columns_for_metadata_fields.rb
@@ -1,39 +1,51 @@
 class CreateRealColumnsForMetadataFields < ActiveRecord::Migration
   def up
-    add_column :users, :first_name,               :string
-    add_column :users, :last_name,                :string
-    add_column :users, :encrypted_password,       :string
-    add_column :users, :remember_created_at,      :datetime
-    add_column :users, :current_sign_in_at,       :datetime
-    add_column :users, :last_sign_in_at,          :datetime
-    add_column :users, :current_sign_in_ip,       :string
-    add_column :users, :last_sign_in_ip,          :string
-    add_column :users, :confirmed_at,             :datetime
-    add_column :users, :confirmation_sent_at,     :datetime
-    add_column :users, :locked_at,                :datetime
-    add_column :users, :reset_password_sent_at,   :datetime
-    add_column :users, :approved_rfc5646_locales, :text
+    # create temporary columns
+    add_column :users, :deprecated__first_name,               :string
+    add_column :users, :deprecated__last_name,                :string
+    add_column :users, :deprecated__encrypted_password,       :string
+    add_column :users, :deprecated__remember_created_at,      :datetime
+    add_column :users, :deprecated__current_sign_in_at,       :datetime
+    add_column :users, :deprecated__last_sign_in_at,          :datetime
+    add_column :users, :deprecated__current_sign_in_ip,       :string
+    add_column :users, :deprecated__last_sign_in_ip,          :string
+    add_column :users, :deprecated__confirmed_at,             :datetime
+    add_column :users, :deprecated__confirmation_sent_at,     :datetime
+    add_column :users, :deprecated__locked_at,                :datetime
+    add_column :users, :deprecated__reset_password_sent_at,   :datetime
+    add_column :users, :deprecated__approved_rfc5646_locales, :text
 
-    execute "ALTER TABLE users ADD CONSTRAINT encrypted_password_exists CHECK (char_length(encrypted_password) > 20);"
-
-    if User.respond_to? :metadata_column_fields
-      deprecated_metadata_columns = User.metadata_column_fields.keys.select { |k| k.to_s.start_with?('deprecated__')}
-
-      User.all.each do |user|
-        deprecated_metadata_columns.each do |depr_col|
-          col = depr_col.to_s.split('deprecated__')[1]
-          user.send :"#{col}=", user.send(depr_col)
-        end
-        user.save!
+    # populate temporary columns
+    deprecated_metadata_columns = User.columns.map(&:name).select {|c| c.start_with?("deprecated__")}
+    User.find_each do |user|
+      metadata = JSON.parse(user.metadata)
+      hsh = {}
+      deprecated_metadata_columns.each do |column_name|
+        hsh[:"#{column_name}"] = metadata[column_name.split("deprecated__").last]
       end
+      user.update_columns hsh
     end
 
+    # rename temporary columns to permanent columns
+    rename_column :users, :deprecated__first_name,               :first_name
+    rename_column :users, :deprecated__last_name,                :last_name
+    rename_column :users, :deprecated__encrypted_password,       :encrypted_password
+    rename_column :users, :deprecated__remember_created_at,      :remember_created_at
+    rename_column :users, :deprecated__current_sign_in_at,       :current_sign_in_at
+    rename_column :users, :deprecated__last_sign_in_at,          :last_sign_in_at
+    rename_column :users, :deprecated__current_sign_in_ip,       :current_sign_in_ip
+    rename_column :users, :deprecated__last_sign_in_ip,          :last_sign_in_ip
+    rename_column :users, :deprecated__confirmed_at,             :confirmed_at
+    rename_column :users, :deprecated__confirmation_sent_at,     :confirmation_sent_at
+    rename_column :users, :deprecated__locked_at,                :locked_at
+    rename_column :users, :deprecated__reset_password_sent_at,   :reset_password_sent_at
+    rename_column :users, :deprecated__approved_rfc5646_locales, :approved_rfc5646_locales
+
+    execute "ALTER TABLE users ADD CONSTRAINT encrypted_password_exists CHECK (char_length(encrypted_password) > 20);"
     change_column_null :users, :first_name, false
     change_column_null :users, :last_name, false
     change_column_null :users, :encrypted_password, false
-  end
 
-  def down
-    raise "Rollback disabled"
+    User.reset_column_information
   end
 end
