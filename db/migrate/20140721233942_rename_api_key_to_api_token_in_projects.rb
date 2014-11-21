c4d80c6c505ddbb65b5533479897c8e07b809c0d
Yunus Sasmaz <yunus@squareup.com>
2014-11-21 01:40:00 -0800

    Copyright updates

--- db/migrate/20140721233942_rename_api_key_to_api_token_in_projects.rb
+++ db/migrate/20140721233942_rename_api_key_to_api_token_in_projects.rb
@@ -1,3 +1,17 @@
+# Copyright 2014 Square Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 class RenameApiKeyToApiTokenInProjects < ActiveRecord::Migration
   def up
     rename_column :projects, :api_key, :api_token # to make it more clear. `key` is a very overloaded term in Shuttle.
