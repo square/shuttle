65aa1b0e2aaa1fc566bf60a402b9a2866212ab8e
Yunus Sasmaz <yunus@squareup.com>
2014-11-23 22:24:40 -0800

    Simplify configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,26 @@
 set :application, 'shuttle'
-set :repo_url, 'https://stash.corp.squareup.com/scm/intl/shuttle.git'
+set :repo_url, 'https://stash.corp.squareup.com/scm/intl/shuttle.git' # Square-specific
 
 set :branch, 'deployable'
 
 set :deploy_to, "/app/#{fetch :application}"
 
 set :linked_files, %w{  config/database.yml
+                        config/environments/common/app.yml
+                        config/environments/common/git.yml
+                        config/environments/common/mailer.yml
+                        config/environments/staging/elasticsearch.yml
+                        config/environments/staging/mailer.yml
                         config/environments/staging/paperclip.yml
+                        config/environments/staging/squash.yml
+                        config/environments/staging/worker.yml
+                        config/environments/production/elasticsearch.yml
+                        config/environments/production/mailer.yml
                         config/environments/production/paperclip.yml
+                        config/environments/production/redis.yml
+                        config/environments/production/squash.yml
                         config/environments/production/stash.yml
+                        config/environments/production/worker.yml
                         data/secret_token
                         tmp/sidekiq.pid  }
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle tmp/repos tmp/working_repos}
