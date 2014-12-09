b4f4ef7ee951ba08335ce5ddf1b79c499a0d0973
Yunus Sasmaz <yunus@squareup.com>
2014-12-08 16:53:34 -0800

    Align whitespaces

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@ worker_boxes = (1..3).map { |i| "user@shuttle-worker#{i.to_s.rjust(2, '0')}.exam
 web_boxes    = (1..2).map { |i| "user@shuttle-web#{i.to_s.rjust(2, '0')}.example.com" }
 db_boxes     = %w{user@shuttle-db.example.com}
 
-role :app, (web_boxes + worker_boxes + db_boxes).uniq
-role :web, web_boxes
-role :db, db_boxes
-role :sidekiq, worker_boxes
+role :app,          (web_boxes + worker_boxes + db_boxes).uniq
+role :web,          web_boxes
+role :db,           db_boxes
+role :sidekiq,      worker_boxes
 role :primary_cron, worker_boxes.first
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,8 +3,8 @@ set :rails_env, :staging
 
 single_server = "user@shuttle-staging.example.com"
 
-role :app,     [single_server]
-role :web,     [single_server]
-role :db,      [single_server]
-role :sidekiq, [single_server]
+role :app,          [single_server]
+role :web,          [single_server]
+role :db,           [single_server]
+role :sidekiq,      [single_server]
 role :primary_cron, [single_server]
