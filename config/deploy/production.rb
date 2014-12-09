c055d41a53841981fa24f3f33606382df6434bfb
Yunus Sasmaz <yunus@squareup.com>
2014-12-08 16:53:01 -0800

    Align whitespaces

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@ set :stage, :production
 worker_boxes = (1..3).map { |i| "square@shuttle-worker#{i.to_s.rjust(2, '0')}.corp.squareup.com" } + (1..5).map { |i| "square@shuttle-worker-a-#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 web_boxes    = (1..2).map { |i| "square@shuttle-web-a-#{i.to_s.rjust(2, '0')}.corp.squareup.com" }
 
-role :app, (web_boxes + worker_boxes).uniq
-role :web, web_boxes
-role :db, worker_boxes[1] # Migrations will be run on this box
-role :sidekiq, worker_boxes
+role :app,          (web_boxes + worker_boxes).uniq
+role :web,          web_boxes
+role :db,           worker_boxes[1] # Migrations will be run on this box
+role :sidekiq,      worker_boxes
 role :primary_cron, worker_boxes.first
