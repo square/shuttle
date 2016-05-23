square/shuttle

commit eac03c6aa205fdf6dc7986a60f52bc74db84b3b3

Migrate Squash to Sentry

config/deploy.rb
@@ -22,11 +22,8 @@ set :deploy_to, "/app/#{fetch :application}"
 set :linked_files, %w{  config/database.yml
                         config/environments/staging/app.yml
                         config/environments/staging/paperclip.yml
-                        config/environments/staging/squash.yml
                         config/environments/production/app.yml
-                        config/environments/production/paperclip.yml
-                        config/environments/production/squash.yml
-                        config/environments/production/stash.yml }
+                        config/environments/production/paperclip.yml }
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle tmp/repos tmp/working_repos}
 
 set :rvm_type, :system
@@ -55,5 +52,3 @@ namespace :deploy do
   after :finishing, 'deploy:restart'
   after :finishing, 'deploy:sidekiq:restart'
 end
-
-before 'deploy:publishing', 'squash:write_revision'

Capfile
@@ -10,8 +10,6 @@ require 'capistrano/bundler'
 require 'capistrano/rails'
 require 'sidekiq/capistrano'
 require 'whenever/capistrano'
-require 'squash/rails/capistrano3'
-require 'squash/javascript/capistrano'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }

Gemfile
@@ -43,6 +43,7 @@ gem 'paperclip', '>= 4.2.2'
 gem 'aws-sdk'
 gem 'execjs'
 gem 'safemode'
+gem 'sentry-raven'
 
 # IMPORTING
 gem 'nokogiri', '>= 1.6.7.2'
@@ -81,6 +82,10 @@ gem 'coffee-rails'
 gem 'uglifier'
 gem 'hogan_assets'
 
+source 'https://rails-assets.org' do
+  gem 'rails-assets-raven-js'
+end
+
 group :development do
   gem 'redcarpet', require: nil
   gem 'yard', require: nil, platform: :mri
@@ -101,11 +106,6 @@ group :development, :test do
   gem 'binding_of_caller'
   gem 'pry'
 end
-
-# ERROR NOTIFICATIONS
-gem 'squash_ruby', require: 'squash/ruby'
-gem 'squash_rails', require: 'squash/rails'
-gem 'squash_javascript', require: 'squash/javascript'
 
 # DEPLOYING
 group :development do

app/assets/javascripts/application.js.coffee
@@ -65,7 +65,8 @@
 #= require selectize
 #= require selectize_with_defaults
 #= require select2
-
+#=
+#= require raven-js
 #
 #= require_tree ../templates
 #= require_tree ./application

app/controllers/application_controller.rb
@@ -124,9 +124,7 @@ class ApplicationController < ActionController::Base
   self.responder = JsonDetailResponder
 
   before_filter :authenticate_user!
-
-  include Squash::Ruby::ControllerMethods
-  enable_squash_client
+  before_filter :set_raven_context
 
   rescue_from(ActiveRecord::RecordNotFound) do
     respond_to do |format|
@@ -255,4 +253,12 @@ class ApplicationController < ActionController::Base
       end
     end
   end
+
+  private
+
+  def set_raven_context
+    return unless user_signed_in?
+    Raven.user_context user_id: current_user.id, email: current_user.email
+  end
+
 end

app/controllers/commits_controller.rb
@@ -177,7 +177,7 @@ class CommitsController < ApplicationController
     flash[:alert] = t('controllers.commits.create.project_not_linked_error', revision: params[:commit][:revision].strip)
     redirect_to root_url
   rescue Timeout::Error
-    Squash::Ruby.notify err, project_id: project_id, sha: sha
+    Raven.capture_exception err, extra: { project_id: project_id, sha: sha }
     flash[:alert] = t('controllers.commits.create.timeout', revision: revision)
     redirect_to root_url
   end

app/views/layouts/application.slim
@@ -49,4 +49,7 @@ html lang="en"
       = raw(ERB.new(File.read(js_erb_file)).result(binding)) if File.exist?(js_erb_file)
       = raw(CoffeeScript.compile(ERB.new(File.read(js_coffee_erb_file)).result(binding))) if File.exist?(js_coffee_erb_file)
 
+    - if ENV['SENTRY_PUBLIC_DSN']
+      script type='text/javascript' Raven.config("#{ENV['SENTRY_PUBLIC_DSN']}").install()
+
     = render partial: 'layouts/flashes'

app/workers/auto_importer.rb
@@ -61,7 +61,7 @@ class AutoImporter
       project.save!
 
     rescue Timeout::Error => err
-      Squash::Ruby.notify err, project_id: project_id
+      Raven.capture_exception err, extra: { project_id: project_id }
       self.class.perform_in 2.minutes, project_id
     end
 

commit 570d05f9938c40605f4939d57918b292428a9478

Fixes regression introduced in eac03c6aa20

config/deploy.rb
@@ -23,7 +23,8 @@ set :linked_files, %w{  config/database.yml
                         config/environments/staging/app.yml
                         config/environments/staging/paperclip.yml
                         config/environments/production/app.yml
-                        config/environments/production/paperclip.yml }
+                        config/environments/production/paperclip.yml
+                        config/environments/production/stash.yml }
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle tmp/repos tmp/working_repos}
 
 set :rvm_type, :system